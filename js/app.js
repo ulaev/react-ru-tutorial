01a79a2e139cf013b51f00bd42489902f3394a27
Alexander Ulaev <ulaev.tlt@gmail.com>
2018-10-17 03:54:53 +0300

    Создание компонента. done

--- js/app.js
+++ js/app.js
@@ -1,15 +1,45 @@
-class App extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-    render() {
-      return (
-        <div>Всем привет, я компонент App!</div>
-      );
-    }
+class Comments extends React.Component {
+  constructor(props) {
+    super(props);
   }
-  
-  ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-  );
+  render() {
+    return (
+      <div className="comments">
+        Нет новостей - комментировать нечего
+      </div>
+    );
+  }
+}
+
+class News extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="news">
+        К сожалению, новостей нет.
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="app">
+        Всем привет, я компонент App! Я умею отображать новости.
+        <News />
+        <Comments />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);

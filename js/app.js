fdc455b160e59653791983169297ef0100d6bf3d
Александр <ulaev.tlt@live.com>
2018-10-19 18:56:48 +0400

    refactoring

--- js/app.js
+++ js/app.js
@@ -1,11 +1,31 @@
-class Comments extends React.Component {
+const my_news = [
+  {
+    author: 'Саша Печкин',
+    text: 'В четверг, четвертого числа...'
+  },
+  {
+    author: 'Просто Вася',
+    text: 'Считаю, что $ должен стоить 35 рублей!'
+  },
+  {
+    author: 'Гость',
+    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+  }
+];
+
+class Article extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
+    const author = this.props.data.author,
+        text = this.props.data.text;
+
     return (
-      <div className="comments">
-        Нет новостей - комментировать нечего
+      <div className='article'>
+        <p className='news__author'>{author}:</p>
+        <p className='news__text'>{text}</p>
       </div>
     );
   }
@@ -16,9 +36,20 @@ class News extends React.Component {
     super(props);
   }
   render() {
+    const data = this.props.data;
+
+    const newsTemplate = data.length > 0 ? data.map(function(item, index) {
+      return (
+        <div key={index}>
+          <Article data={item} />
+        </div>
+      )
+    }) : <p>К сожалению новостей нет</p>
+
     return (
       <div className="news">
-        К сожалению, новостей нет.
+        {newsTemplate}
+        <strong className={data.length > 0 ? '':'none'}>Всего новостей: {data.length}</strong>
       </div>
     );
   }
@@ -31,9 +62,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="app">
-        Всем привет, я компонент App! Я умею отображать новости.
-        <News />
-        <Comments />
+        <h3>Новости</h3>
+        <News data={my_news}/>
       </div>
     );
   }
